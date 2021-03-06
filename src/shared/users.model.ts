370b7f56ae05f6e13cefff1a1572dc8e0dd0704b
HipsterSantos <santoscampos269@gmail.com>
2021-03-06 02:18:39 +0100

    pusing file to remote repo, getting shape of refactoring

---
+++ src/shared/users.model.ts
@@ -0,0 +1,27 @@
+export interface Users{
+    login:string;
+    avatar_url:string;
+    gravatar_id:string;
+    node_id:string;
+    id:number
+    totalContribution:number;
+    site_admin:boolean;
+    score:number;
+    url:string;
+    html_url:string;
+    followers_url:string;
+    following_url:string;
+    gists_url:string;
+    subscriptions_ur:string;
+    organizations_url:string;
+    starred_url?:string;
+    repos_url:string;
+    events_url:string;
+    received_events_url:string;
+    contributionCollection?:{obj:Object}
+    type: GithubAccount.USER
+}
+enum GithubAccount{
+    USER="User",
+    ORG="Organization"
+}
